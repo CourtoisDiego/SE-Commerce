772d5b889a48f3e8de8c60df41ee1d43111ac4c7
CourtoisDiego <dfrivera026@gmail.com>
2020-09-24 18:59:08 -0600

    Clase24092020

--- bckEcommerce/routes/index.js
+++ bckEcommerce/routes/index.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/info',function(req,res){
+    var infoObject = {};
+    infoObject.cuenta="0801199918960";
+    infoObject.nombre="Diego Fernando Cortez";
+    infoObject.email="dfrivera026@gmail.com";
+    res.json(infoObject);
+});//Aca termina
+
+router.get('/hook',function(req,res){
+  res.render("hook",{});
+});//get hook
+
+router.post('/hook', function(req,res){
+  var body= req.body;
+  console.log(body);
+  res.render("hook",body);
+});
+
 module.exports = router;
+
